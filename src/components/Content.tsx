a7ff4169054d278cf3e5b270b5a764ec6b915857
Shubham Patrick <ahthneeuhl@gmail.com>
2023-07-16 19:27:40 +0530

    feat: create DraftCard components

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -3,7 +3,7 @@ import Switcher from "./Switcher";
 
 const Content: React.FC = () => {
   return (
-    <Box height="100%">
+    <Box height="100%" sx={{ boxShadow: "0 0 2px 0 rgba(0, 0, 0, 0.07)" }}>
       <Switcher />
     </Box>
   );
--- src/components/Outreach.tsx
+++ src/components/Outreach.tsx
@@ -1,18 +1,22 @@
 import { OpenInBrowserOutlined } from "@mui/icons-material";
-import { Button, Stack } from "@mui/material";
+import { Box, Button, Stack } from "@mui/material";
+import DraftCard from "./DraftCard";
 
 const Outreach: React.FC = () => {
   return (
-    <Stack justifyContent="center" alignItems="center">
-      <Button
-        variant="contained"
-        sx={{ borderRadius: 16, textTransform: "none", padding: "8px 24px" }}
-        startIcon={<OpenInBrowserOutlined />}
-        disableElevation
-      >
-        Generate Outreach Mail
-      </Button>
-    </Stack>
+    <Box>
+      <Stack justifyContent="center" alignItems="center">
+        <Button
+          variant="contained"
+          sx={{ borderRadius: 16, textTransform: "none", padding: "8px 24px" }}
+          startIcon={<OpenInBrowserOutlined />}
+          disableElevation
+        >
+          Generate Outreach Mail
+        </Button>
+      </Stack>
+      <DraftCard />
+    </Box>
   );
 };
 
