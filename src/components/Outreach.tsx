32b6af3edba2fdf74c0f18917a467e1da22c3481
Shubham Patrick <ahthneeuhl@gmail.com>
2023-07-17 12:13:08 +0530

    feat: render drafts with global state

--- src/components/Outreach.tsx
+++ src/components/Outreach.tsx
@@ -1,9 +1,20 @@
 import { OpenInBrowserOutlined } from "@mui/icons-material";
-import { Box, Button, Stack } from "@mui/material";
-import User from "./User";
+import { Box, Button, Stack, Typography } from "@mui/material";
+import { useAppDispatch, useAppSelector } from "redux/store/hooks";
 import DraftList from "./DraftList";
+import User from "./User";
+import { setOutreachDrafts } from "redux/slices/outreach";
 
 const Outreach: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const outreachDrafts = useAppSelector(
+    (state) => state.outreach.outreachDrafts
+  );
+
+  const handleGenerateOutreach = () => {
+    dispatch(setOutreachDrafts(true));
+  };
+
   return (
     <Box>
       <User
@@ -18,11 +29,18 @@ const Outreach: React.FC = () => {
           sx={{ borderRadius: 16, textTransform: "none", padding: "8px 24px" }}
           startIcon={<OpenInBrowserOutlined />}
           disableElevation
+          onClick={handleGenerateOutreach}
         >
           Generate Outreach Mail
         </Button>
       </Stack>
-      <DraftList />
+      {outreachDrafts ? (
+        <DraftList />
+      ) : (
+        <Typography color="#5f5f5f" fontSize="14px" textAlign="center" py={4}>
+          Please Generate Outreach Mail
+        </Typography>
+      )}
     </Box>
   );
 };
